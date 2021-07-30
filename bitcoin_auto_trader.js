b3926d99ee1cd3ca03ad988df9da2d322c247bd9
Anderson Bassan <bassan.programador@gmail.com>
2021-07-30 00:31:07 -0300

    add basic extension structure and create a function that verify if bitpreco is open

--- bitcoin_auto_trader.js
+++ bitcoin_auto_trader.js
@@ -1,3 +1,75 @@
-window.onload = () => {
-	console.log(`Pretent that it has a bunch of wholesome code here...`)
+let isBitprecoOpen = async (tabs) => {
+	/* function that verify if bitpreco is open */
+
+	/* gets a promisse the tabs of the open windows in the browser */
+	let querying_tabs = browser.tabs.query({});
+
+	let is_bitpreco_open = await querying_tabs.then(
+		/* if the promise succeeds then */
+		
+		(tabs) => {
+			let open_tabs = [];
+
+			/* add the url of the tabs to open tabs list */
+			for (let tab of tabs) {
+				open_tabs.push(tab.url);
+			}
+
+			/* return the current state of bitpreco as a boolean promisse */
+			if (open_tabs.includes('https://market.bitpreco.com/trade')) {
+				return true;	
+			
+			} else {
+				return false;
+			};
+
+		}).catch(
+			/* if it fails then log the error message (kinda useless) */
+			(e) => {console.log(e)}
+
+		);
+
+	return is_bitpreco_open;
+
+
+};
+
+let openBitpreco = () => {
+	/* insert wholesome code here */
 }
+
+let getBitcoinPrice = () => {
+	/* insert wholesome code here */
+
+}
+
+let playAudio = () => {
+	/* insert wholesome code here */
+
+}
+
+let main = () => {
+	/* the desired price to play a sound when reached */
+	let desired_price = 190000;
+
+	let verifing_bitpreco_open = isBitprecoOpen()
+		.then(is_bitpreco_open => {
+		/* If bitpreco is not open opens it */
+
+		if (! is_bitpreco_open ) {
+			console.log('opening bitpreco')
+			openBitpreco();
+		
+		}
+	});
+
+	/* after opening bitpreco if it's not open get the bitcoin price and if it equal or bellow the deired price play a sound */
+	verifing_bitpreco_open
+	.finally( e => {
+		if (getBitcoinPrice() < desired_price) {
+			playAudio();
+		}
+	});
+}
+
+main()
