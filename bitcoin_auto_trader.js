1a4cb3113698f50b2db9bd0b3f28070d5d8e2ea3
Anderson Bassan <bassan.programador@gmail.com>
2021-08-03 04:12:42 -0300

    Refactors code so it works. Now it gets the value of the bitcoin from the site

--- bitcoin_auto_trader.js
+++ bitcoin_auto_trader.js
@@ -1,77 +1,70 @@
+let openBitpreco = async (tabs) => {
+	console.log('\t\t\tinside openbitpreco...');
+
+	browser.tabs.create({url: 'https://market.bitpreco.com/'});
+
+	return true;
+}
+
 let isBitprecoOpen = async (tabs) => {
-	/* function that verify if bitpreco is open */
 
-	/* gets a promisse the tabs of the open windows in the browser */
-	let querying_tabs = browser.tabs.query({});
+	/* creates a promisse that searchs for bitpreco tabs */
+	let querying_tabs = browser.tabs.query({url: 'https://market.bitpreco.com/*'});
 
-	let is_bitpreco_open = await querying_tabs.then(
-		/* if the promise succeeds then */
+	/* returns true if it finds any open bitpreco tabs, false otherwise */
+	return querying_tabs.then((tab) => {
+
+		/* if the number of open tabs in https://market.bitpreco.com is bigger than 0 return true */
+		return tab.length > 0 ? true : false; 
+
+	}).catch((err) => {
 		
-		(tabs) => {
-			let open_tabs = [];
+		/* if querying tabs fails logs a message */
+		console.log(err);
 
-			/* add the url of the tabs to open tabs list */
-			for (let tab of tabs) {
-				open_tabs.push(tab.url);
-			}
+	});
 
-			/* return the current state of bitpreco as a boolean promisse */
-			if (open_tabs.includes('https://market.bitpreco.com/trade')) {
-				return true;	
-			
-			} else {
-				return false;
-			};
+}
 
-		}).catch(
-			/* if it fails then log the error message (kinda useless) */
-			(e) => {console.log(e)}
+let getBitcoinPrice = async (tabs) => {
 
-		);
+	/* verifies if bitpreco is open */
+	let guarantees_bitpreco_is_open =  isBitprecoOpen().then(is_open => {
+		if (!is_open ) {
+			return openBitpreco().then(e => e);
+		} else {
+			return true;
+		}
+	
+	});
 
-	return is_bitpreco_open;
+	let gets_bitpreco_tab_id =  guarantees_bitpreco_is_open.then(() => {
+		let gets_bitpreco_tab_id = browser.tabs.query({url: 'https://market.bitpreco.com/*'}).then(tab => tab[0]);
 
+		return gets_bitpreco_tab_id;
+	});
+
+	let get_bitcoin_price = gets_bitpreco_tab_id.then((tab, tabs) => {
+		// let hilights_tab = browser.tabs.highlight({tabs: [tab.index],windowId: tab.WindowId});
+		// hilights_tab.then(e => console.log(e));
+
+		let get_current_btc_price_code = `document.querySelectorAll('#root div div header div div div:nth-of-type(2) div')[6].innerText.slice(16).replace('.', '').replace(',', '.');`;
+
+		let run_code_in_tab = browser.tabs.executeScript(tab.id, {code: get_current_btc_price_code});
+		
+		run_code_in_tab.then(e => {return e});
+
+		return run_code_in_tab;
+
+	});
+
+	return get_bitcoin_price;
 
 };
 
-let openBitpreco = (tabs) => {
-	/* insert wholesome code here */
+let main = async (tabs) => {
+	getBitcoinPrice().then(e => console.log('get bitcoin price -> ', e[0]));
 
-	/* opens bitpreco in a new tab */
-	browser.tabs.create({url: 'https://market.bitpreco.com/trade'});
-}
+};
 
-let getBitcoinPrice = () => {
-	/* insert wholesome code here */
-
-}
-
-let playAudio = () => {
-	/* insert wholesome code here */
-
-}
-
-let main = () => {
-	/* the desired price to play a sound when reached */
-	let desired_price = 190000;
-
-	let verifing_bitpreco_open = isBitprecoOpen()
-		.then(is_bitpreco_open => {
-		/* If bitpreco is not open opens it */
-
-		if (! is_bitpreco_open ) {
-			openBitpreco();
-		
-		}
-	});
-
-	/* after opening bitpreco if it's not open get the bitcoin price and if it equal or bellow the deired price play a sound */
-	verifing_bitpreco_open
-	.finally( e => {
-		if (getBitcoinPrice() < desired_price) {
-			playAudio();
-		}
-	});
-}
-
-main()
+main();
